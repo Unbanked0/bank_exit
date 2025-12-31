b1e06d5286f7c0528127f2a226d4cdf1c0140270
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2025-12-31 19:13:41 +0100

    fix: hide layout and component items on print

--- app/helpers/hotkeys_helper.rb
+++ app/helpers/hotkeys_helper.rb
@@ -3,13 +3,13 @@ module HotkeysHelper
     # kbd-xs / kbd-sm
     klass = "kbd kbd-#{size} text-base-content hide-on-touch"
 
-    content_tag :span, class: 'hide-on-touch' do
+    content_tag :span, class: 'hide-on-touch print:hidden' do
       if hotkey.is_a?(Array)
         hotkey.map do |key|
           tag.kbd(key.capitalize, class: klass)
-        end.join('&nbsp;+&nbsp;').html_safe
+        end.join('&nbsp;+&nbsp;').html_safe # rubocop:disable Rails/OutputSafety
       else
-        tag.kbd(hotkey.capitalize, class: klass).html_safe
+        tag.kbd(hotkey.capitalize, class: klass).html_safe # rubocop:disable Rails/OutputSafety
       end
     end
   end
