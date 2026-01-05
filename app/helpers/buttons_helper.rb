40933f1635e058f0f86e2eece542c5d00d352e42
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2026-01-05 16:57:39 +0100

    refactor(style): various header, nav, map and directories sidebar enhancements

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -109,7 +109,7 @@ module ButtonsHelper
     kbd = build_hotkey_kbd(hotkey, **options.slice(:size))
     content_for :hotkey, kbd, flush: true if kbd
 
-    link_to(url, **options.slice(:data, :class, :id)) do
+    link_to(url, **options.slice(:data, :class, :id, :title)) do
       if block_given?
         content = capture(&block)
 
