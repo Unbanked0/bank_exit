bfd0305298bf795617918cffc975459861879296
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2026-07-21 10:16:53 +0200

    fix: broken monero tutorial title missing in homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -15,6 +15,8 @@ class WelcomeController < PublicController
     @profiles = Questions::BuildProfiles.call
     @levels = Questions::BuildLevels.call
 
+    @monero_tutorial = Tutorial.find('monero-nokyc')
+
     merchants_sample = Merchant.available.monero.in_france.no_kyc.includes(:logo_attachment, :banner_attachment).sample(4)
     @merchants_sample = MerchantDecorator.wrap(merchants_sample)
   end
