9eb14c8ae4f60c251890434f349a3964209c5d8b
Truman Blurbank <209260142+TrumanBlurbank@users.noreply.github.com>
2026-08-08 15:42:27 +0200

    fix(country): add missing Kosovo (`XK` country code)

--- config/initializers/country_select.rb
+++ config/initializers/country_select.rb
@@ -1,3 +1,15 @@
+# NOTE: Kosovo does not have an ISO 3166-1 alpha-2 code yet and as such
+# is not included in the "countries" gem.
+ISO3166::Data.register(
+  alpha2: 'XK',
+  alpha3: 'XKX',
+  name: 'Kosovo',
+  currency: 'EUR',
+  translations: {
+    'en' => 'Kosovo'
+  }
+)
+
 CountrySelect::FORMATS[:with_flag] = lambda do |country|
   label = country.translations&.send(:[], I18n.locale.to_s) || country.common_name || country.iso_short_name
 
